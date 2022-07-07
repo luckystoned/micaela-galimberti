d9688bf9a815df6a7f1b34dff646f94cff4e4432
luckystoned <luckystoned.dev@gmail.com>
2022-07-08 00:09:50 +0200

    contact  data ok / still missing style and interac

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,8 +7,46 @@ import Seo from "../components/Seo"
 const Contact = () => (
   <Layout>
     <Seo title="Contact" />
-    <h1>Contact Page</h1>
-    <Link to="/">Homepage</Link>
+    <div className="know-me px-5">
+      <h1>Contact</h1>
+      <div className="my-6">
+        <p>
+        Let’s talk, tell me
+        about your project
+        or inquire.
+        </p>
+      </div>
+      <div className="my-6">
+        <span>
+          EMAIL <br/>
+        </span>
+        <span>
+          mica.galimberti@gmail.com 
+        </span>
+      </div>
+      <div className="my-6">
+        <span>
+          WHATS APP ME <br/>
+        </span>
+        <span>
+          (+) 54 11 2391 1872
+        </span>
+      </div>
+      <div className="my-6">
+        <span>
+          SOCIAL MEDIA <br/>
+        </span>
+        <span>
+          (Be) /micagalimberti <br/>
+        </span>
+        <span>
+          (Li) /micaelagalimberti <br/>
+        </span>
+        <span>
+          (Ig) /its.micki
+        </span>
+      </div>
+    </div>
   </Layout>
 )
 
