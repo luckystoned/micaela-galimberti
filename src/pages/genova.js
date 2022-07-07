3cee73225b9b9302ee3a3fdd2a7d2371fe46984a
luckystoned <luckystoned.dev@gmail.com>
2022-07-07 23:44:26 +0200

    genova project data ok / still missing style and interac

--- src/pages/genova.js
+++ src/pages/genova.js
@@ -19,14 +19,26 @@ const Genova = () => (
         <button type="button">Edit</button>
       </div>
       <div className="flex flex-col">
-          <StaticImage src="../images/genova1.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
-          <StaticImage src="../images/genova2.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
-          <StaticImage src="../images/genova3.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
-          <StaticImage src="../images/genova4.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
-          <StaticImage src="../images/genova5.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
-          <StaticImage src="../images/genova6.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
-          <StaticImage src="../images/genova7.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
-          <StaticImage src="../images/genova8.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
+        <h3>Génova Pizzeria</h3>
+        <span>( Redesign of the brand )</span>
+        <h4>Rebranding</h4>
+        <span>          
+          Jan. 2022 <br/>
+          Buenos Aires, Argentina
+        </span>
+          
+          <ul>
+            <li>• Illustration.</li>
+            <li>• Brand identity.</li>
+          </ul>
+        <StaticImage src="../images/genova1.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
+        <StaticImage src="../images/genova2.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
+        <StaticImage src="../images/genova3.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
+        <StaticImage src="../images/genova4.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
+        <StaticImage src="../images/genova5.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
+        <StaticImage src="../images/genova6.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
+        <StaticImage src="../images/genova7.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
+        <StaticImage src="../images/genova8.jpg" alt="Génova Pizzeria" layout="fullWidth"/>
       </div>
     </div>
   </Layout>
