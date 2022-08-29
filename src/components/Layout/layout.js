f0e466c7350e6618696d052821f700afe34ba7e7
luckystoned <luckystoned.dev@gmail.com>
2022-08-29 20:24:43 -0300

    test fix layout heigh

--- src/components/Layout/layout.js
+++ src/components/Layout/layout.js
@@ -29,9 +29,9 @@ const Layout = ({ children }) => {
 
   return (
     <div className={currentPage}>
-      <div className="container">
+      <div className="flex flex-col min-h-screen">
         <Header siteTitle={title || `Title`} />
-        <main>{children}</main>
+        <main className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">{children}</main>
         <Footer />
       </div>
     </div>
